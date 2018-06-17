a9ebf106cbee6ba4f2e522da6f2d3d3739a1e80d
Alfian Fadli Pramadhan <34793564+AlfianFadliPramadhan@users.noreply.github.com>
2018-06-17 23:54:17 +0000

    Add files via upload

---
+++ Arduino Proteus.ino
@@ -0,0 +1,41 @@
+    // defines pins numbers
+    const int trigPin = 9;
+    const int echoPin = 10;
+    // defines variables
+    long duration;
+    int distance;
+    
+void setup() {
+    pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+    Serial.begin(9600); // Starts the serial communication
+    }
+
+void loop() {
+  int pressurePin = 0;
+  int val = analogRead(pressurePin);      // read the rawADC value.
+  float voltage = (float) val / 1023.0 * 5.0;   // voltage at the input pin
+  float kPa = ((voltage - 0.135) / 0.0459) + 15;
+
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    distance= duration*0.034/2;
+    // Prints the distance on the Serial Monitor
+    Serial.print("distance>");
+    Serial.print(distance);//distance
+    Serial.print(" pressure>");
+    Serial.print(kPa);//pressure
+    Serial.print(" latitude>");
+    Serial.print(-8.021);
+    Serial.print(" longitude>");
+    Serial.println(110.321);
+    delay(100);
+}
---
+++ Arduino WIFI.ino
@@ -0,0 +1,67 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char *ssid = "MyESP8266AP";
+const char *password = "testpassword";
+ 
+WiFiServer server(80);
+
+String inputString = "";
+String dt = "";
+
+String header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+String html_1 = "<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"1\"><title>Web SCADA</title></head><body>";
+String html_2 = "<h1>Peringatan Dini Tsunami</h1>";
+String html_3 = "<h2>Data Sensor</h2><br>";
+String html_4 = "</body></html>";
+   
+void setup() {
+
+  Serial.begin(9600);
+ 
+  WiFi.softAP(ssid, password);
+
+  server.begin();
+ 
+}
+ 
+void loop() {
+
+  WiFiClient client = server.available();
+  
+    client.print( header );
+    client.print( html_1 );
+    client.print( html_2 );
+    client.print( html_3 );
+
+    if (Serial.available() > 0) {
+    dt = "";
+    char inChar = (char)Serial.read();
+    inputString += inChar;
+    if (inChar == '\n') {
+      client.print("<h3>" + inputString + "</h3>");
+      dt = inputString;
+
+      client.print( html_4 );
+
+      delay(1);
+      client.flush();
+    } 
+  } else {
+    if (dt != "") {
+      client.print("<h3>" + dt + "</h3>");
+      inputString = "";
+
+      client.print( html_4 );
+
+      delay(1);
+      client.flush();
+
+    }
+  }
+    
+    client.print( html_4 );
+ 
+    delay(1);
+    client.flush();
+}
